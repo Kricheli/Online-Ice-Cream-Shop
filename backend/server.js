f41c5dd74988353fa261eae0faa5af99f4a9d83f
Kricheli <krichelieden@gmail.com>
2022-07-13 14:03:29 +0300

    removing the hard-coded port

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,13 @@
 const express = require('express')
+const dotenv = require('dotenv')
+// const dotenv = require('dotnev')
 const products = require('./data/products')
+
+dotenv.config()
+// dotenv.config()
+
+const PORT = process.env.PORT || 5000
+// const PORT = process.env.PORT || 5000
 
 const app = express()
 app.get('/', (req, res) => {
@@ -13,7 +21,12 @@ app.get('/api/products/:id', (req, res) => {
   const product = products.find((p) => p._id === req.params.id)
   res.json(product)
 })
-app.listen(5000, console.log('Srever running on port 5000'))
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+)
+// app.listen(PORT, console.log(`Srever running in ${process.env.NODE_ENV} on port ${PORT}`))
 
 //First we want to set up the server so we will use express because it's far more fast and works with js which is a very easy language to learn.
 // afterwards we set up the home page by the command app.get and add the url to route the http request to the path we specified, then we need to add a callback function using res & req (response and request) to describe we want to do in that page.
