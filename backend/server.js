f856090ed522b0a3ee6b278c74b5ef91e374f0cf
Kricheli <krichelieden@gmail.com>
2022-07-15 23:34:16 +0300

    accidently added a function to the server.js instead of productRoutes.js

--- backend/server.js
+++ backend/server.js
@@ -14,13 +14,6 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
-// app.get('/api/products', (req, res) => {
-//   res.json(products)
-// })
-// app.get('/api/products/:id', (req, res) => {
-//   const product = products.find((p) => p._id === req.params.id)
-//   res.json(product)
-// })
 
 const PORT = process.env.PORT || 5000
 
