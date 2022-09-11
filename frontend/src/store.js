393981991572bc9ad3cf7a28ad5c49ca9c2a853d
Kricheli <krichelieden@gmail.com>
2022-09-11 19:12:21 +0300

    adding redux

---
+++ frontend/src/store.js
@@ -0,0 +1,16 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { poroductListReducer } from './reducers/productReducers'
+
+const reducer = combineReducers({
+  productList: poroductListReducer,
+})
+const initialState = {}
+const middleware = [thunk]
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+)
+export default store
