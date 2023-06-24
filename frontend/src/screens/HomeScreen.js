e0b18f25d4349d75959b86a6bacbefc52fd0cc95
Kricheli <krichelieden@gmail.com>
2023-06-24 14:50:07 +0300

    adding the map function to homeScreen.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -59,8 +59,15 @@ const HomeScreen = ({ match }) => {
                 </video>
                 </div >
       <h1 className='header mt-2'>Our Products</h1>
-          
-          
+          <Row>
+            {products.map((product) => {
+              return (
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3} className='align-middle'>
+                  <Product product={product} />
+                </Col>
+              )
+            })}
+          </Row>
           <Paginate
             pages={pages}
             page={page}
