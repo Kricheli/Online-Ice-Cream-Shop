2e1a05b8dd2bbe2b187b99088147dd7bb1ff198b
Kricheli <krichelieden@gmail.com>
2023-06-24 14:52:02 +0300

    adding ternary expression to the map function

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -60,9 +60,9 @@ const HomeScreen = ({ match }) => {
                 </div >
       <h1 className='header mt-2'>Our Products</h1>
           <Row>
-            {products.map((product) => {
+            {products?.map((product) => {
               return (
-                <Col key={product._id} sm={12} md={6} lg={4} xl={3} className='align-middle'>
+                <Col key={product?._id} sm={12} md={6} lg={4} xl={3} className='align-middle'>
                   <Product product={product} />
                 </Col>
               )
