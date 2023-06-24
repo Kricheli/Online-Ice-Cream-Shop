47b214d5ef61de4a508753f33d62362895235309
Kricheli <krichelieden@gmail.com>
2023-06-24 21:42:51 +0300

    returning to the previous homeScreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,18 +1,17 @@
-import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import Product from '../components/Product.js'
 import { Row, Col } from 'react-bootstrap'
-import Product from '../components/Product'
 import Message from '../components/Message'
-import Loader from '../components/Loader'
-import Paginate from '../components/Paginate'
-// import ProductCarousel from '../components/ProductCarousel'
-// import Meta from '../components/Meta'
-import { listProducts } from '../actions/productActions'
+import { listProducts } from '../actions/productAction'
+import Loader from '../components/Loader.js'
+import Paginate from '../components/Paginate.js'
+import Video from '../media/bg-g.mp4'
+import VideoHeader from '../media/video-header.mp4'
+import '../index.css'
 
 const HomeScreen = ({ match }) => {
   const keyword = match.params.keyword
-
   const pageNumber = match.params.pageNumber || 1
 
   const dispatch = useDispatch()
@@ -26,28 +25,51 @@ const HomeScreen = ({ match }) => {
 
   return (
     <>
-      <Meta />
-      {!keyword ? (
-        <ProductCarousel />
-      ) : (
-        <Link to='/' className='btn btn-light'>
-          Go Back
-        </Link>
-      )}
-      <h1>Latest Products</h1>
       {loading ? (
         <Loader />
       ) : error ? (
         <Message variant='danger'>{error}</Message>
       ) : (
         <>
-          <Row>
-            {products.map((product) => (
-              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} />
-              </Col>
-            ))}
-          </Row>
+          <div className='saleContainer'>
+            <p className='sale'>free shipping on orders over $50</p>
+          </div>
+          <div className='videoContainer'>
+            <video id='video' src={Video} autoPlay loop muted playsInline>
+              Sorry, your browser doesn't support embedded videos,
+            </video>
+          </div>
+          <div className='videoHeaderContainer'>
+            <video
+              id='videoHeader'
+              src={VideoHeader}
+              autoPlay
+              loop
+              muted
+              playsinline
+            >
+              Sorry, your browser doesn't support embedded videos,
+            </video>
+          </div>
+          <h1 className='header mt-2'>Our Products</h1>
+          {products ? (
+            <Row>
+              {products?.map((product) => {
+                return (
+                  <Col
+                    key={product._id}
+                    sm={12}
+                    md={6}
+                    lg={4}
+                    xl={3}
+                    className='align-middle'
+                  >
+                    <Product product={product} />
+                  </Col>
+                )
+              })}
+            </Row>
+          ) : null}
           <Paginate
             pages={pages}
             page={page}
